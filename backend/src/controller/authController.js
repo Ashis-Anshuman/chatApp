af1163c1780e5fe382e8dd95b61e4d75113dbf2a
Ashis Anshuman <sahoolikun850@gmail.com>
2025-11-28 14:16:20 +0530

    Add signUp

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,37 +1,50 @@
-import User from "./models/userModel.js";
+import User from "../models/userModel.js";
+import { generateToken } from "../lib/webToken.js";
 import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res)=>{
-    const {fullName, email, password} = req.body
+    const {fullName, email, password} = req.body;
+    // console.log(fullName);
+    // console.log(email);
+    // console.log(password);
 
     try {
         if(!fullName || !email || !password){
-        return res.staus(400).json({message : "All field are required!!"});
+        return res.status(400).json({message : "All field are required!!"});
         }
 
         if(password.length<8){
-            return res.staus(400).json({message : "Password atleast 8 character"});
+            return res.status(400).json({message : "Password atleast 8 character"});
         }
 
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!regex.test(email)){
-            return res.staus(400).json({message : "Invalid email"})
+            return res.status(400).json({message : "Invalid email"})
         }
 
         const existingUser = await User.findOne({email:email});
         if(existingUser){
-            return res.staus(400).json({message : "Existing user"});
+            return res.status(400).json({message : "Existing user"});
         }
 
         const salt = await bcrypt.genSalt(10)
-        const hashPass = await bcrypt.hash(password,salt)
+        const hashPass = await bcrypt.hash(password,salt)  //password hashing
         const newUser = new User({
             fullName, 
             email, 
             password: hashPass
         });
         if(newUser){
+            generateToken(newUser._id, res)  //webtoken generated
             newUser.save();
+
+            return res.status(201).json({
+                message: "User has created",
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email
+            })
+
         }else{
             return res.status(400).json({message:"Invalid user data"})
         }
--- backend/src/routers/authRoute.js
+++ backend/src/routers/authRoute.js
@@ -1,10 +1,9 @@
 import express from "express"
+import { signUp } from "../controller/authController.js"; 
 
 const router = express.Router();
 
-router.get("/signup",(req, res)=>{
-    res.send("signUp");
-})
+router.post("/signup",signUp)
 
 router.get("/login", (req, res)=>{
     res.send("login");
