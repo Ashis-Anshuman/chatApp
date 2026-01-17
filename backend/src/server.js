6e156210af205421ae78d6828a8ee4f075a12e21
Ashis Anshuman <sahoolikun850@gmail.com>
2026-01-17 22:46:31 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const PORT = process.env.PORT||3000;
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(cookieParser());
 const __dirname = path.resolve();
